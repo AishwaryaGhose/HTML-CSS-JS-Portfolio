d8c47b92339662eda7827228ad19f013b456c0e7
Aishwarya Ghose <50588750+AishwaryaGhose@users.noreply.github.com>
2024-09-24 19:22:15 +0600

    Finished Web Portal

---
+++ script.js
@@ -0,0 +1,6 @@
+function Togglemenu(){
+    const menu = document.querySelector(".hamburger-menu-links");
+    const icon = document.querySelector(".hanburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
